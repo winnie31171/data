50b7098a2c4c9bf714dc5c5f1fc49c0cafb08c8a
winnie31171 <b04608023@ntu.edu.tw>
2016-11-16 18:52:39 +0800

    applejs

---
+++ apple.js
@@ -0,0 +1,32 @@
+function fruit()
+{
+<script type="text/javascript">
+var width = 1024;
+var height = 768;
+var ctrl = d3.select("body").append("svg").attr("width", width).attr("height", height);
+d3.csv("https://github.com/winnie31171/data/blob/master/apple2013.csv", 
+	function(data)
+	{
+		var ln = data.length;
+		console.log(data);
+		var maxy = d3.max(data, function(d){ return d.Value; });
+		var lines = d3.line().x(function(d,i){return i*(width/ln);}).y(function(d){return height-d.Value*(height/maxy)});
+		ctrl.append("path").attr("d", function(d){
+			return "M0,"+height+"L"+width+","+height;
+		}).attr("stroke", "black").attr("fill", "none");
+		ctrl.append("path").attr("d", function(d){
+			return "M0,0"+"L0,"+height;
+		}).attr("stroke", "black").attr("fill", "none");
+		ctrl.append("path").data([data]).attr("d", lines).attr("stroke", "red").attr("fill", "none");
+		x.domain(d3.extent(data, function(d) { return d.date; }));
+		y.domain([0, maxy]);
+		ctrl.append("g")
+      	.attr("transform", "translate(0," + height + ")")
+      	.call(d3.axisBottom(x));
+  		ctrl.append("g")
+      	.call(d3.axisLeft(y));
+	}
+
+);
+</script>
+}
